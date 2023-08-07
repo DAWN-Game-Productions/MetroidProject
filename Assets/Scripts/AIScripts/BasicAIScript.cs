675bfcde75e80969993d2fdc0e0cb4809321d1a6
Mark Rizko <mark@rizkocircle.com>
2023-08-07 16:22:29 -0700

    Add and display player health bar

--- Assets/Scripts/AIScripts/BasicAIScript.cs
+++ Assets/Scripts/AIScripts/BasicAIScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class BasicAIScript : MonoBehaviour
 {
+
+    public HealthBar enemyHealth;
+    public int maxHealth = 100;
+    public int currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        enemyHealth.setMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyHealth.setHealth(currentHealth);
     }
 }
---
+++ Assets/Scripts/PlayerScripts/HealthBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void setHealth(int health)
+    {
+        slider.value = health;
+    }
+
+
+    public void setMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    // change max health without healing
+    public void changeMaxHealth(int health)
+    {
+        slider.maxValue = health;
+    }
+}
